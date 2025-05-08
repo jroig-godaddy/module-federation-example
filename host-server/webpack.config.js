d992b1f2fe1f533116c3685370b29d2206eadf4f
jon roig <jroig@godaddy.com>
2025-05-08 10:46:12 -0700

    remove config

--- host-server/webpack.config.js
+++ host-server/webpack.config.js
@@ -40,9 +40,7 @@ module.exports = {
             name: 'consuming_app',
             shared: {
               react: { singleton: true, requiredVersion: '*', eager: true, strictVersion: false },
-              'react-dom': { singleton: true, requiredVersion: '*',  eager: true, strictVersion: false },
-              'superagent': { singleton: true, requiredVersion: '*',  eager: true, strictVersion: false }
-            },
+              'react-dom': { singleton: true, requiredVersion: '*',  eager: true, strictVersion: false }            },
           }),
 
         new HtmlWebpackPlugin({
